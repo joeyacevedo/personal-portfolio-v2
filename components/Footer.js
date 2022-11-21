14bec9ee5c4a9565b632829c7d0f0288e1015cf4
Joey Acevedo <thejosephacevedo@gmail.com>
2022-11-20 22:30:39 -0500

    Footer top border added

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-black bg-opacity-60 h-[40px] text-white grid place-items-center w-screen bottom-0">
+    <div className="bg-black bg-opacity-60 h-[40px] text-white grid place-items-center w-screen bottom-0 border-t-4 border-black">
       <div className="text-center text-sm bottom-0">
         <p>Created with Next.js & tailwindcss | Joseph Acevedo 2022 &copy;</p>
       </div>
